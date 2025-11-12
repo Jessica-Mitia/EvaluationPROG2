f332c9e11469e10fd89c553721024aed3cb5522b
Mitia Jessica <mitiah.jessica@gmail.com>
2025-11-12 16:20:32 +0300

    feat: ajout de la classe Etudiant

--- src/main/java/Etudiant.java
+++ src/main/java/Etudiant.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Etudiant {
+import java.time.LocalDate;
+
+@Getter
+@Setter
+
+public class Etudiant extends Personne{
+    private String groupe;
+    private Tuteur tuteur;
+
+    public Etudiant(int id, String nom, String prenom, LocalDate dateNaissance, String email, String telephone, String groupe, Tuteur tuteur) {
+        super(id, nom, prenom, dateNaissance, email, telephone);
+        this.groupe = groupe;
+        this.tuteur = tuteur;
+    }
+
+
 }
