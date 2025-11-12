f78fac75d83f00de6732ce1b65c7d40e3717bf61
Mitia Jessica <mitiah.jessica@gmail.com>
2025-11-12 16:21:37 +0300

    feat: ajout de la classe Cours

--- src/main/java/Cours.java
+++ src/main/java/Cours.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 
 public class Cours {
+    public enum Label {
+        PROG1, PROG2, PROG3, WEB1, WEB2
+    }
+
+    private int id;
+    private Label label;
+    private int credit;
+    private Enseignant enseignant;
 }
