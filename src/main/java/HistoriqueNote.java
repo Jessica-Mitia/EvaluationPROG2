758295f355215848d9ebc126a9b7249031f9d98c
Mitia Jessica <mitiah.jessica@gmail.com>
2025-11-17 21:56:53 +0300

    feat: ajout de la classe HistoriqueNote

--- src/main/java/HistoriqueNote.java
+++ src/main/java/HistoriqueNote.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@AllArgsConstructor
+@Getter
+@Setter
 
 public class HistoriqueNote {
+    private Instant debut;
+    private double nouvelleValeur;
+    private String raison;
 }
