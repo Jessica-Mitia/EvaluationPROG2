8bb994f106d44bf13da35661e5815ea3247636f4
Mitia Jessica <mitiah.jessica@gmail.com>
2025-11-17 21:56:33 +0300

    feat: ajout de la classe Note

--- src/main/java/Note.java
+++ src/main/java/Note.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
 
 public class Note {
+    private Etudiant etudiant;
+    private double valeurInitiale;
+    private List<HistoriqueNote> historiqueNotes;
 }
